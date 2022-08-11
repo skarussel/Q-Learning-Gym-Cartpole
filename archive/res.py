79bbcb0b37396fe3c86cec602a33fcbab40ff0b5
skarussel <steven.kocadag@st.ovgu.de>
2022-08-11 19:02:39 +0300

    update structure

--- archive/res.py
+++
@@ -1,8 +0,0 @@
-from email import header
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.read_csv("results.csv", delimiter="\t")
-print(df)
-df.plot.line("i_episode", "Reward")
-plt.show()
